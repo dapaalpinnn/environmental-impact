9463e930368ec3b4093877f25ae2144f3e8d583e
Dafa Alvin <dafaalvin10@gmail.com>
2025-01-25 14:35:29 +0700

    style(animation): added scroll animation with motion library

--- src/components/Impacts.jsx
+++ src/components/Impacts.jsx
@@ -1,13 +1,13 @@
 import { environmentImpactItems } from "../utils/environment";
 import CoreConcepts from "./CoreConcepts";
 import { motion } from "motion/react";
-import { imageAnimation } from "../utils/motionVariant";
+import { imageAnimation, fadeIn } from "../utils/motionVariant";
 import plantImage from "../assets/plant.svg";
 
 export default function Impacts() {
   return (
     <section className="font-manrope max-w-sm px-4 mx-auto py-12 rounded-3xl sm:max-w-xl sm:mt-12">
-      <div className="flex items-center gap-4">
+      <motion.div {...fadeIn} className="flex items-center gap-4">
         <h1 className="text-3xl tracking-tight font-semibold">Impacts</h1>
         <motion.img
           animate={imageAnimation}
@@ -15,11 +15,11 @@ export default function Impacts() {
           alt="Plant"
           className="w-12 origin-bottom-left mb-4 sm:w-32 sm:mt-16"
         />
-      </div>
-      <p className="text-sm text-stone-600 font-medium dark:text-stone-300">
+      </motion.div>
+      <motion.p {...fadeIn} className="text-sm text-stone-600 font-medium dark:text-stone-300">
         Recognizing these challenges is the first step toward adopting
         sustainable practices to protect the Earth for future generations.
-      </p>
+      </motion.p>
       <div className="grid gap-4 grid-cols-1 text-sm mt-4">
         {environmentImpactItems.map((item) => (
           <CoreConcepts key={item.title} {...item} />
--- src/components/atoms/TabButton.jsx
+++ src/components/atoms/TabButton.jsx
@@ -1,3 +1,6 @@
+import { motion } from "motion/react";
+import { fadeIn } from "../../utils/motionVariant";
+
 export default function TabButton({
   children,
   isSelected = false,
@@ -5,13 +8,14 @@ export default function TabButton({
   ...props
 }) {
   return (
-    <button
+    <motion.button
+      {...fadeIn}
       {...props}
       className={`${
         isSelected ? "bg-stone-800 text-stone-200" : color
       } px-4 py-2 ring-1 ring-stone-700 text-stone-700 rounded-lg font-manrope font-medium text-sm hover:text-stone-900 hover:ring-stone-900 dark:bg-transparent dark:text-stone-300 dark:ring-stone-300 dark:hover:ring-stone-100 dark:hover:text-stone-100`}
     >
       {children}
-    </button>
+    </motion.button>
   );
 }
